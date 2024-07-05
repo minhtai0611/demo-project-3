009a0f1f001ac91167ebd21e946618120d87cc8b
Tai Minh <minhtainth06111999@gmail.com>
2024-07-05 07:19:43 +0700

    demo-project-3

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, {ErrorRequestHandler} from "express";
 // import { createHandler } from "graphql-http/lib/use/express";
 // import { ruruHTML } from "ruru/server";
 import cors from "cors";
@@ -12,5 +12,19 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
+
+// app.all("/graphql", createHandler())
+
+// app.get("/", (req, res) => {
+//     res.type("html");
+//     res.end(ruruHTML({ endpoint: "/graphql" }));
+// })
+
+const errorMiddleware: ErrorRequestHandler = (err, req, res, next) => {
+    console.error(err.message);
+    res.status(500).send("Server encountered an unexpected edition and got error");
+}
+
+app.use(errorMiddleware);
 
 app.listen(PORT, () => console.log(`Server NodeJS + Express is running on port ${PORT}`));
